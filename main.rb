f4f7763a911bac996f7e4cf53cfe98f4f4424ab3
ahardencoopers <ahardencoopers@gmail.com>
2017-07-14 11:55:15 -0500

    Agregar pagina de busqueda

--- main.rb
+++ main.rb
@@ -8,6 +8,10 @@ get '/' do
 	return 'Busca las claves del catálogo ClaveProdServ para el CFDI 3.3 <br> <a href=http://104.236.245.141:4567/buscar?descripcion=Software">http://http://104.236.245.141:4567/buscar?descripcion=Software</a> <br> Más información en <a href="https://github.com/ahardencoopers/API-ClaveProdServ-CFDI3.3">https://github.com/ahardencoopers/API-ClaveProdServ-CFDI3.3</a>'
 end
 
+get '/busqueda' do
+	send_file('public/busqueda.html')
+end
+
 get '/clave/:clave' do
 	return get_clave(params[:clave].to_s)
 end
